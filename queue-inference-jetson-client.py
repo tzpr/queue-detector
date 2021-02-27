af2139e03942b79f5ef9a49c52a08739c1a8bceb
Toni Räsänen <toni.rasanen@helsinki.fi>
2021-02-27 20:13:52 +0200

    handle detections

--- queue-inference-jetson-client.py
+++ queue-inference-jetson-client.py
@@ -9,17 +9,29 @@ import jetson.utils
 # connect to redis
 message_broker = redis.Redis(host='localhost', port=6060)
 
-net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
-camera = jetson.utils.videoSource("/dev/video0")  # V4L2 webcam
+net = jetson.inference.detectNet('ssd-mobilenet-v2', threshold=0.5)
+camera = jetson.utils.videoSource('/dev/video0')  # V4L2 webcam
 
 
 def analyze_queue_picture():
+    number_of_persons = 0
+    PERSON_CLASS_ID = 1
+    queue_state = 'no queue'
+
     img = camera.Capture()
     detections = net.Detect(img)
 
+    for idx in detections:
+        if detections[idx].ClassID == PERSON_CLASS_ID:
+            number_of_persons = number_of_persons + 1
+
     # https://realpython.com/python-string-formatting/
-    print(f'DEBUG the detections: {detections}')
-    return 'daadaa'
+    print(f'DEBUG number of persons: {number_of_persons}')
+
+    if number_of_persons > 1:
+        queue_state = 'queue'
+
+    return queue_state
 
 
 def check_queue():
